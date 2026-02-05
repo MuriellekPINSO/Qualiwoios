78564cbbe18a0ab4d89bdcea56ab5a319631b3f7
Muri <muriellekpinso@gmail.com>
2026-02-05 21:06:35 +0100

    Initial Commit

---
+++ Qualiwo/QualiwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  QualiwoApp.swift
+//  Qualiwo
+//
+//  Created by Murielle KPINSO  on 05/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct QualiwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
